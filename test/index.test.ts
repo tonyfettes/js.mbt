093b1afb7be7a1356e6a1c9dc732e7938a2cae9c
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-02-08 09:43:41 +0800

    test: Value::is_null

--- test/index.test.ts
+++ test/index.test.ts
@@ -14,7 +14,7 @@ async function loadWasm(imports: WebAssembly.Imports) {
   } else {
     const wasmUrl = new URL(
       "target/wasm/debug/build/test.wasm",
-      import.meta.url,
+      import.meta.url
     );
     return WebAssembly.instantiateStreaming(fetch(wasmUrl), imports);
   }
@@ -27,31 +27,22 @@ export type MoonBitJsTestExports = {
   test_array_modify: (array: number[]) => undefined;
   test_int: (value: number) => number;
   test_int_option: (value: number | undefined) => number;
+  test_is_null: (value: null) => boolean;
 };
 
-test("wasm", async () => {
+test("test", async () => {
   const { instance } = await loadWasm(JsImports);
-  const exports = instance.exports as MoonBitJsTestExports;
-  expect(exports.test_array_int()).toMatchObject([3]);
-  expect(exports.test_array_float()).toMatchObject([1, 2, 3]);
-  expect(exports.test_array_double()).toMatchObject([1, 2, 3]);
-  const array = [1, 2, 3];
-  exports.test_array_modify(array);
-  expect(array).toMatchObject([1, 2, 3, 4]);
-  expect(exports.test_int(42)).toBe(42);
-  expect(exports.test_int_option(42)).toBe(42);
-  expect(exports.test_int_option(undefined)).toBe(-1);
-});
-
-test("js", async () => {
-  const exports = JsExports;
-  expect(exports.test_array_int()).toMatchObject([3]);
-  expect(exports.test_array_float()).toMatchObject([1, 2, 3]);
-  expect(exports.test_array_double()).toMatchObject([1, 2, 3]);
-  const array = [1, 2, 3];
-  exports.test_array_modify(array);
-  expect(array).toMatchObject([1, 2, 3, 4]);
-  expect(exports.test_int(42)).toBe(42);
-  expect(exports.test_int_option(42)).toBe(42);
-  expect(exports.test_int_option(undefined)).toBe(-1);
+  const wasmExports = instance.exports as MoonBitJsTestExports;
+  for (const exports of [wasmExports, JsExports]) {
+    expect(exports.test_array_int()).toMatchObject([3]);
+    expect(exports.test_array_float()).toMatchObject([1, 2, 3]);
+    expect(exports.test_array_double()).toMatchObject([1, 2, 3]);
+    const array = [1, 2, 3];
+    exports.test_array_modify(array);
+    expect(array).toMatchObject([1, 2, 3, 4]);
+    expect(exports.test_int(42)).toBe(42);
+    expect(exports.test_int_option(42)).toBe(42);
+    expect(exports.test_int_option(undefined)).toBe(-1);
+    expect(exports.test_is_null(null)).toBe(true);
+  }
 });
